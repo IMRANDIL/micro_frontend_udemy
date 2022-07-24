cc63188439b5a8ca29fc8c4eca1ae9b741efb21c
IMRANDIL <aliimranadil2@gmail.com>
2022-07-24 14:52:09 +0530

    yh

--- ecom/container/src/bootstrap.js
+++ ecom/container/src/bootstrap.js
@@ -1,4 +1,6 @@
-import "products/ProductsIndex";
-import "cart/CartShow";
+import { mount as Productmount } from "products/ProductsIndex";
+import { mount as Cartmount } from "cart/CartShow";
 
 console.log("container");
+Productmount(document.querySelector("#dev-products"));
+Cartmount(document.querySelector("#dev-cart"));
