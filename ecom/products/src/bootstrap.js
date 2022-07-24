d24d67c1b5d5c37173b51b99ce3624eb9bf15739
IMRANDIL <aliimranadil2@gmail.com>
2022-07-24 14:11:54 +0530

    yh

--- ecom/products/src/bootstrap.js
+++ ecom/products/src/bootstrap.js
@@ -9,3 +9,23 @@ for (let i = 0; i < 5; i++) {
 }
 
 document.querySelector("#dev-products").innerHTML = products;
+
+//Context/Situation #1
+
+// We are runnning this file in development in isolation..
+
+//we are using our local index.html file..
+
+//which definitely has an element with an id of 'dev-products'
+
+// we want to immediately render our app into that element
+
+//Context/situation #2..
+
+//we are running this file in development or production
+
+//through the container app
+
+//no guarantee that an element with an id of 'dev-products' exits..
+
+//we do not want to immediately ender the app..
